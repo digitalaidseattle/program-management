91efb6e266216a3a2294219c091f2d09cf96e4ed
jnakaso <jnakaso@yahoo.com>
2024-08-16 11:44:21 -0700

    pm key

--- src/services/airtableClient.ts
+++ src/services/airtableClient.ts
@@ -8,6 +8,6 @@ import Airtable from 'airtable';
 
 const dasAirtableClient = new Airtable({ apiKey: import.meta.env.VITE_AIRTABLE_PAT })
 
-const pmAirtableClient = new Airtable({ apiKey: import.meta.env.VITE_AIRTABLE_PAT })
+const pmAirtableClient = new Airtable({ apiKey: import.meta.env.VITE_AIRTABLE_PM_ANON_KEY })
 
 export { dasAirtableClient, pmAirtableClient };
