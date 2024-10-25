52fa5d61f16ed4fb3297ac905ef8179eaa64bac6
jnakaso <jnakaso@yahoo.com>
2024-10-25 11:24:05 -0700

    card action area

--- src/pages/evaluations/index.tsx
+++ src/pages/evaluations/index.tsx
@@ -2,6 +2,7 @@
 // material-ui
 import {
   Card,
+  CardActionArea,
   CardMedia,
   Stack,
   Typography,
@@ -38,28 +39,32 @@ const VentureCard: React.FC<VentureProps> = ({ venture }) => {
 
   return (
     <Card sx={{ display: 'flex', padding: '2' }} onClick={() => navigate(`/evaluation/${venture.id}`)}>
-      <CardMedia
-        component='img'
-        image={venture.imageSrc ? venture.imageSrc : placeholder}
-        alt={venture.title + " logo"}
-        sx={{
-          objectFit: 'contain',
-          width: { md: '7rem', lg: 200 },
-          aspectRatio: '1 / 1',
-          borderRadius: '8px',
-          display: { xs: 'none', md: 'block' },
-          backgroundColor: 'white',
-        }}
-      />
-      <Stack margin={1} spacing={1}>
-        <Stack direction={'row'} spacing={{ xs: 1, sm: 2, md: 4 }}>
-          <Typography variant='h5'>{venture.title} </Typography>
-          <Typography color={statusColor(venture)}>{venture.status}</Typography>
+      <CardActionArea>
+        <Stack direction={'row'}>
+          <CardMedia
+            component='img'
+            image={venture.imageSrc ? venture.imageSrc : placeholder}
+            alt={venture.title + " logo"}
+            sx={{
+              objectFit: 'contain',
+              width: { md: '7rem', lg: 200 },
+              aspectRatio: '1 / 1',
+              borderRadius: '8px',
+              display: { xs: 'none', md: 'block' },
+              backgroundColor: 'white',
+            }}
+          />
+          <Stack margin={1} spacing={1}>
+            <Stack direction={'row'} spacing={{ xs: 1, sm: 2, md: 4 }}>
+              <Typography variant='h5'>{venture.title} </Typography>
+              <Typography color={statusColor(venture)}>{venture.status}</Typography>
+            </Stack>
+            <Typography >{venture.partner}</Typography>
+            <Typography >{venture.painpoint}</Typography>
+            <Typography >{venture.description}</Typography>
+          </Stack>
         </Stack>
-        <Typography >{venture.partner}</Typography>
-        <Typography >{venture.painpoint}</Typography>
-        <Typography >{venture.description}</Typography>
-      </Stack>
+      </CardActionArea>
     </Card>
   )
 };
