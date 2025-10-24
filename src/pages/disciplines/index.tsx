0631193f3f22ed3664ff3a998d6a65849061cdf1
jnakaso <jnakaso@yahoo.com>
2025-10-24 07:43:40 -0700

    small bug fixes

--- src/pages/disciplines/index.tsx
+++ src/pages/disciplines/index.tsx
@@ -13,32 +13,32 @@ import { DisciplineDetails } from '../discipline';
 
 const storageService = useStorageService()!;
 
-  const columns: GridColDef<Discipline[][number]>[] = [
-    {
-      field: 'icon',
-      headerName: '',
-      width: 100,
-      renderCell: (params) => (
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            width: "100%",
-            height: "100%",
-          }}
-        >
-          <Avatar
-            src={storageService.getUrl(`icons/${params.row.id}`)}
-            alt={`${params.row.name} icon`}
-            sx={{ width: 40, height: 40, objectFit: 'contain' }}
-            variant="rounded"
-          />
-        </Box>
-      ),
-    },
-    { field: 'name', headerName: 'Name', width: 200 },
-    { field: 'description', headerName: 'Description', width: 800 },
+const columns: GridColDef<Discipline[][number]>[] = [
+  {
+    field: 'icon',
+    headerName: '',
+    width: 100,
+    renderCell: (params) => (
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <Avatar
+          src={supabaseStorage.getUrl(`icons/${params.row.id}`)}
+          alt={`${params.row.name} icon`}
+          sx={{ width: 40, height: 40, objectFit: 'contain' }}
+          variant="rounded"
+        />
+      </Box>
+    ),
+  },
+  { field: 'name', headerName: 'Name', width: 200 },
+  { field: 'details', headerName: 'Details', width: 800 },
 
 ];
 
