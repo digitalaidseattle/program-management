3b370260dab267274b94b37f6983604df01dde65
jnakaso <jnakaso@yahoo.com>
2025-11-04 08:38:39 -0800

    merge fixes

--- src/components/ManagedListCard.tsx
+++ src/components/ManagedListCard.tsx
@@ -1,6 +1,6 @@
 import { PlusCircleOutlined } from "@ant-design/icons";
 import { SelectItemDialog } from "@digitalaidseattle/mui";
-import { Card, CardContent, CardHeader, Grid, IconButton, Paper, Stack, Typography } from "@mui/material";
+import { Card, CardContent, CardHeader, Grid, IconButton, Paper, Stack } from "@mui/material";
 import { ReactNode, useState } from "react";
 
 export type ManagedListCardProps = {
