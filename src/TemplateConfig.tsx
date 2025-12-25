592d0ef7cefd19fed85ab8022e5d41ac77fe2dd6
jnakaso <jnakaso@yahoo.com>
2025-12-24 21:34:16 -1000

    clean up github workflow; clean up dashboard

--- src/TemplateConfig.tsx
+++ src/TemplateConfig.tsx
@@ -1,27 +1,12 @@
 
 
 import {
-    CalendarOutlined,
-    CompassOutlined,
-    CrownOutlined,
-    HeartOutlined,
-    HomeOutlined,
-    IdcardOutlined,
-    KeyOutlined,
-    MehOutlined,
-    PaperClipOutlined,
-    RocketOutlined,
-    ScheduleOutlined,
-    SearchOutlined,
-    TeamOutlined,
-    ToolOutlined,
-    UserOutlined
+    HomeOutlined
 } from '@ant-design/icons';
 import logo from "./assets/images/logo-light-icon.svg";
 
 import { LayoutConfiguration, MenuItem } from "@digitalaidseattle/mui";
 import packageJson from '../package.json';
-import { Link } from 'react-router-dom';
 
 export const TemplateConfig = () => {
     const home =
@@ -37,57 +22,6 @@ export const TemplateConfig = () => {
                 url: '/',
                 icon: <HomeOutlined />
             }]
-    }
-
-    const meetings =
-    {
-        id: 'meetins-group',
-        title: 'Meetings',
-        type: 'group',
-        children: [
-            {
-                id: 'team',
-                title: 'All Meetings',
-                type: 'item',
-                url: '/all-meetings',
-                icon: <CompassOutlined />
-            },
-            {
-                id: 'plenary',
-                title: 'Plenary',
-                type: 'item',
-                url: '/plenary',
-                icon: <ScheduleOutlined />
-            },
-            {
-                id: 'leadership',
-                title: 'Leadership',
-                type: 'item',
-                url: '/leadership',
-                icon: <CrownOutlined />
-            }]
-    }
-
-    const ventures = {
-        id: 'venture-dashboard',
-        title: 'Ventures',
-        type: 'group',
-        children: [
-            {
-                id: 'reporting',
-                title: 'Reporting',
-                type: 'item',
-                url: '/reporting',
-                icon: <PaperClipOutlined />
-            },
-            {
-                id: 'staffing',
-                title: 'Staffing',
-                type: 'item',
-                url: '/staffing',
-                icon: <MehOutlined />
-            }
-        ]
     } as MenuItem;
 
 
@@ -211,48 +145,47 @@ export const TemplateConfig = () => {
         ]
     } as MenuItem;
 
-    const recruiting = {
-        id: 'recruiting-dashboard',
-        title: 'Recruiting',
-        type: 'group',
-        children: [
-            {
-                id: 'applicants',
-                title: 'Applicants',
-                type: 'item',
-                url: '/applicants',
-                icon: <SearchOutlined />
-            },
-            {
-                id: 'proctors',
-                title: 'Proctors',
-                type: 'item',
-                url: '/proctors',
-                icon: <MehOutlined />
-            }
-        ]
-    } as MenuItem;
-
+    // const recruiting = {
+    //     id: 'recruiting-dashboard',
+    //     title: 'Recruiting',
+    //     type: 'group',
+    //     children: [
+    //         {
+    //             id: 'applicants',
+    //             title: 'Applicants',
+    //             type: 'item',
+    //             url: '/applicants',
+    //             icon: <SearchOutlined />
+    //         },
+    //         {
+    //             id: 'proctors',
+    //             title: 'Proctors',
+    //             type: 'item',
+    //             url: '/proctors',
+    //             icon: <MehOutlined />
+    //         }
+    //     ]
+    // } as MenuItem;
 
     return ({
         appName: 'DAS Program Management',
         logoUrl: logo,
         drawerWidth: 240,
-        menuItems: [home, meetings, ventures, recruiting, reference, data],
+        menuItems: [home], // meetings, ventures, recruiting, reference, data],
         toolbarItems: [],
         profileItems: [
-            <Link
-                style={{ 'textDecoration': 'none' }}
-                color="secondary"
-                to={`/profile`}>
-                Profile
-            </Link>,
-            <Link
-                style={{ 'textDecoration': 'none' }}
-                color="secondary"
-                to={`/privacy`}>
-                Privacy Policy
-            </Link>
+            // <Link
+            //     style={{ 'textDecoration': 'none' }}
+            //     color="secondary"
+            //     to={`/profile`}>
+            //     Profile
+            // </Link>,
+            // <Link
+            //     style={{ 'textDecoration': 'none' }}
+            //     color="secondary"
+            //     to={`/privacy`}>
+            //     Privacy Policy
+            // </Link>
         ],
         version: packageJson.version,
     } as LayoutConfiguration);
