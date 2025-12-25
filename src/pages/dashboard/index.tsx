bc166532180944606a8a9ff5192af98f1e0ee880
jnakaso <jnakaso@yahoo.com>
2025-12-24 21:34:16 -1000

    clean up github workflow; clean up dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,23 +1,11 @@
 import { Card, CardContent, CardHeader, Stack } from "@mui/material";
-import Markdown from "react-markdown";
 import { SchedulingWidget } from "../../components/SchedulingWidget";
-import { MyMeetingsWidget } from "../../components/MyMeetingsWidget";
 
 const DashboardPage = () => {
-    const hometext = `
-# This dashboard page is under construction
-
-You should expect to find things like:
-- notifications
-- shortcuts to tasks
-- summaries
-- widgets
-`
 
     return (
         <>
-            <Stack direction='row' gap={2}>
-                <MyMeetingsWidget />
+            <Stack gap={2}>
                 <Card>
                     <CardHeader title="Create Scheduling Links" />
                     <CardContent>
@@ -25,9 +13,6 @@ You should expect to find things like:
                     </CardContent>
                 </Card>
             </Stack>
-            <Markdown>
-                {hometext}
-            </Markdown>
         </>
     );
 };
