37dfcd904813ad265cb91dea5c1cb96e700f17fc
jnakaso <jnakaso@yahoo.com>
2025-12-27 09:28:59 -1000

    add venture report - custom toolbar

--- src/pages/reporting/index.tsx
+++ src/pages/reporting/index.tsx
@@ -1,21 +1,32 @@
-import { LoadingContext, useNotifications } from '@digitalaidseattle/core';
-import { PageInfo, QueryModel } from '@digitalaidseattle/supabase';
+/**
+ *  reporting/index.tsx
+ *
+ *  @copyright 2025 Digital Aid Seattle
+ *
+ */
+import { useContext, useEffect, useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 import {
   Button,
   Card,
   CardContent,
   CardHeader,
+  IconButton,
   Stack,
   Toolbar
 } from '@mui/material';
 import { GridColDef } from '@mui/x-data-grid';
+import { PlusCircleOutlined } from '@ant-design/icons';
 import dayjs from 'dayjs';
-import { useContext, useEffect, useState } from 'react';
-import { Link as RouterLink } from 'react-router-dom';
+
+import { LoadingContext, useNotifications } from '@digitalaidseattle/core';
+import { PageInfo, QueryModel } from '@digitalaidseattle/supabase';
+
 import { EntityTable } from '../../components/EntityTable';
 import { HEALTH_STATUS_CHIPS } from '../../components/StatusChip';
-import { VentureReport, VentureReportService } from '../../services/dasVentureReportService';
 import { VentureReportDetails } from '../../components/VentureReportDetails';
+import { VentureReport, VentureReportService } from '../../services/dasVentureReportService';
+import VentureReportDialog from '../../components/VentureReportDialog';
 
 const ReportingPage = () => {
   const ventureReportService = VentureReportService.instance();
@@ -25,6 +36,8 @@ const ReportingPage = () => {
   const [pageInfo, setPageInfo] = useState<PageInfo<VentureReport>>({ rows: [], totalRowCount: 0 });
   const [queryModel, setQueryModel] = useState<QueryModel>();
   const [selectedReport, setSelectedReport] = useState<VentureReport>();
+
+  const [showAddDialog, setShowAddDialog] = useState<boolean>(false);
 
   useEffect(() => {
     fetchData();
@@ -61,7 +74,9 @@ const ReportingPage = () => {
       field: 'reporting_date', headerName: 'Reporting Date',
       renderCell: (params) => (
         dayjs(params.row.reporting_date).format('MM/DD/YYYY')
-      )
+      ),
+      width: 150
+
     },
     {
       field: 'status', headerName: 'Status', renderCell: (params) => {
@@ -97,6 +112,15 @@ const ReportingPage = () => {
       .then(report => setSelectedReport(report!));
   }
 
+  function toolbar() {
+    return (
+      <Stack direction='row' alignItems={'center'}>
+        <Toolbar>
+          <IconButton color='primary' onClick={() => setShowAddDialog(true)}><PlusCircleOutlined /></IconButton>
+        </Toolbar>
+      </Stack>
+    );
+  }
   return (
     <Card>
       <CardHeader
@@ -108,6 +132,7 @@ const ReportingPage = () => {
             pageInfo={pageInfo}
             onChange={setQueryModel}
             columns={columns}
+            toolbar={toolbar}
             onRowDoubleClick={(evt) => changeSelectedReport(evt.id)}
           />
         ) : (
@@ -126,6 +151,10 @@ const ReportingPage = () => {
             )}
           </Stack>
         )}
+        <VentureReportDialog
+          title={'Add Venture Report'}
+          onClose={() => setShowAddDialog(false)}
+          open={showAddDialog} />
       </CardContent>
     </Card >
   );
